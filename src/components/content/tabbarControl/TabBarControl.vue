14094d079a3a866abf238ded2841add1eaf67a3a
lihaibozi <340063567@qq.com>
2020-05-14 11:01:47 +0800

    '0214'

---
+++ src/components/content/tabbarControl/TabBarControl.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tablControl">
+    <div v-for="(item,index) in titles" :key="item.index" class="tabControlItem"
+         :class="{active:index===currentIndex}" @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "TabBarControl",
+        props:{
+          titles:{
+            type:Array,
+            default(){
+              return []
+            }
+          }
+        },
+      data(){
+          return{
+            currentIndex:0
+          }
+      },
+      methods:{
+        itemClick(index){
+          this.currentIndex = index;
+          this.$emit('tabControl',index);
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .tablControl{
+    display: flex;
+    text-align: center;
+    font-size: 15px;
+    height: 40px;
+    line-height: 40px;
+  }
+  .tabControlItem{
+    flex: 1;
+
+  }
+  .active{
+    color:var(--color-high-text);
+  }
+  .tabControlItem span{
+    padding: 5px;
+  }
+  .active span{
+    border-bottom: 3px solid var(--color-tint);
+  }
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,42 +4,111 @@
       <div   slot="center">购物街</div>
     </navbar>
     <home-swiper :banners="banners"/>
-  <home-recommand-vie :recommends="recommends"/>
+    <home-recommand-vie :recommends="recommends"/>
+    <feature-view/>
+    <tab-bar-control :titles="['流行','新款','精选']" class="tab-control"
+                    @tabControl="tabControl"/>
+    <goods-list :goods="showGoods"/>
+
   </div>
 </template>
 
 <script>
-  import navbar from 'components/common/navbar/navbar'
-  import {getHomeMultidata} from 'network/homerequest'
   import HomeSwiper from './childComps/homeswiper.vue'
   import HomeRecommandVie from './childComps/HomeRecommandView.vue'
+  import FeatureView from './childComps/FeatureView.vue'
+
+  import navbar from 'components/common/navbar/navbar'
+  import TabBarControl from 'components/content/tabbarControl/TabBarControl'
+  import GoodsList from 'components/content/Goods/GoodsList'
 
 
-    export default {
+  import {getHomeMultidata} from 'network/homerequest'
+  import {getHomeGoods} from 'network/homerequest'
+
+
+  export default {
         name: "home",
         components:{
           navbar,
           HomeSwiper,
-          HomeRecommandVie
+          HomeRecommandVie,
+          FeatureView,
+          TabBarControl,
+          GoodsList
         },
       data(){
           return{
             banners:[],
-            recommends:[]
+            recommends:[],
+            goods:{
+              'pop':{page:0,list:[]},
+              'new':{page:0,list:[]},
+              'sell':{page:0,list:[]},
+            },
+            currentType:'pop'
           }
       },
-      created(){
-          getHomeMultidata().then(res =>{
-           this.banners = res.data.banner.list;
-           this.recommends= res.data.recommend.list;
-          })
+    computed:{
+      showGoods(){
+        return this.goods[this.currentType].list
       }
+    },
+      created(){
+        this.getHomeMultidata()
+        this.getHomeGoods('pop')
+        this.getHomeGoods('new')
+        this.getHomeGoods('sell')
+      },
+    methods:{
+       //事件监听
+      tabControl(index){
+        switch(index){
+          case 0:
+            this.currentType='pop'
+            break
+          case 1:
+            this.currentType='new'
+            break
+          case 2:
+            this.currentType='sell'
+            break
+        }
+      },
+      getHomeMultidata(){
+        getHomeMultidata().then(res =>{
+          this.banners = res.data.banner.list;
+          this.recommends= res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type){
+        const page = this.goods[type].page+1;
+        getHomeGoods(type,page).then(res=>{
+          this.goods[type].list.push(...res.data.list);
+          this.goods[type].page += 1;
+        })
+      }
+    }
     }
 </script>
 
 <style scoped>
+    #home{
+      padding-top: 44px;
+    }
     .home-nav{
       background-color: var(--color-tint);
       color: #ffffff;
+      position: fixed;
+      left: 0;
+      right: 0;
+      top: 0;
+      z-index: 9;
     }
+  .tab-control{
+    position: sticky;
+    top: 44px;
+    z-index: 9;
+    background-color:#ffffff;
+  }
 </style>
